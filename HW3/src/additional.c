7afcb1cccf6b24be04e7a003ca4cbd80c3e0be51
Andrew DeMaria <ademaria@mines.edu>
2013-02-12 14:39:49 -0700

    Some handy string function(s) here

---
+++ HW3/src/additional.c
@@ -0,0 +1,16 @@
+#include "additional.h"
+
+void append(char s, char** string){
+    char* old = *string;
+    char* new = (char*) malloc( (strlen(old) + 2) * sizeof( char ) );
+    strncpy( new, old, strlen( old ));
+    new[strlen(old)] = s;
+    new[strlen(old) + 1] = '\0';
+    free( old );
+    old = new;
+    new = NULL;
+}
+
+//void append(char s, char** string){
+//    strncat( *string, &s, 1);
+//}
---
+++ HW3/src/additional.h
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  additional.h
+ *
+ *    Description:  Some additional string funness
+ *
+ *        Version:  1.0
+ *        Created:  02/12/2013 02:24:21 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Andrew DeMaria (muff1nman), ademaria@mines.edu
+ *   Organization:  Colorado School of Mines
+ *
+ * =====================================================================================
+ */
+
+#ifndef ADDITIONAL_H
+#define ADDITIONAL_H
+#include <string.h>
+#include <stdlib.h>
+
+void append(char s, char** string);
+
+#endif
