5b2a2b356eae9a33be90d700fd2e5ae9ef859a56
Andrew DeMaria <ademaria@mines.edu>
2013-02-27 23:35:21 -0700

    removed more junk from the main file

--- HW5/main.rb
+++ HW5/main.rb
@@ -2,25 +2,6 @@
 require_relative 'parse.rb'
 # a simple script to parse a sentence with some rules
 
-def run (doubleHash, start, stack )
-    while hasNonTerminal(start) do
-        # record index of nonTerminal
-        # look up in doubleHash
-        # if it is there, replace it with its value based whats on the stack
-        #   and remove from the stack all values that were consumed
-
-        nonTerminal = getNonTerminal(start)
-        if doubleHash.key? nonTerminal
-            hash = doubleHash[nonTerminal]
-            expandTo = hash[stack[0]]
-            start = replaceNonTerminal( start, expandTo )
-        else 
-            puts "Not sure whats going on here"
-        end
-    end
-    start
-end
-
 #abort("Please suply the string to parse") if ARGV.length != 1
 if ARGV.length != 1
     print "Please enter a string: "
