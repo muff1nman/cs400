36f8909f4776e80e255c983c8649b093d122994e
Andrew DeMaria <ademaria@mines.edu>
2013-03-12 21:57:15 -0600

    Skeleton test cases

---
+++ HW7/test_complex.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require 'test/unit'
+
+class TestComplex < Test::Unit::TestCase
+
+    def setup
+        `make`
+    end
+
+    def test_Real_natural
+
+    end
+
+    def test_Real_decimal
+
+    end
+
+    def test_Real_integer
+
+    end
+
+    def test_imaginary
+
+    end
+
+end
