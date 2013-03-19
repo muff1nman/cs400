c49721022058981ab2921b60058dbad6e0933658
Andrew DeMaria <ademaria@mines.edu>
2013-03-19 08:35:59 -0600

    Empty functions failing tests

---
+++ MidtermReview/even_odd.rkt
@@ -0,0 +1,10 @@
+#!/usr/bin/env racket
+#lang racket
+
+(provide filter-even)
+(define (filter-even given_list)
+  "Not implemented")
+
+(provide filter-odd)
+(define (filter-odd given_list)
+  "Not implemented")
