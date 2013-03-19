f98505efbb3c1ee276cd49b34d40921ccb0345b0
Andrew DeMaria <ademaria@mines.edu>
2013-03-19 09:15:15 -0600

    More c---r functions, i understand now

--- MidtermReview/cddar.rkt
+++ MidtermReview/cddar.rkt
@@ -1,5 +1,21 @@
 #!/usr/bin/env racket
 #lang racket
 
+(display "The list:\n")
+(display '((Alicia Brenda Charles)(100 Main Street)(Golden CO 80401)))
+
+(display "cddar\n")
 (display (cddar '((Alicia Brenda Charles)(100 Main Street)(Golden CO 80401))))
+(display "\n")
+
+(display "caar\n")
+(display (caar '((Alicia Brenda Charles)(100 Main Street)(Golden CO 80401))))
+(display "\n")
+
+(display "cadr\n")
+(display (cadr '((Alicia Brenda Charles)(100 Main Street)(Golden CO 80401))))
+(display "\n")
+
+(display "caddr\n")
+(display (caddr '((Alicia Brenda Charles)(100 Main Street)(Golden CO 80401))))
 (display "\n")
