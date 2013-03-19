1a3e7228dd5f529fb8e3180017cc2c69c238d84b
Andrew DeMaria <ademaria@mines.edu>
2013-03-19 08:50:17 -0600

    Implemented filter-even function

--- MidtermReview/even_odd.rkt
+++ MidtermReview/even_odd.rkt
@@ -3,7 +3,11 @@
 
 (provide filter-even)
 (define (filter-even given_list)
-  "Not implemented")
+  (cond
+    [(empty? given_list) (list)]
+    [(equal? (length given_list) 1) (list) ]
+    [(>= (length given_list) 2) 
+     (list* (first (rest given_list)) (filter-even (rest (rest given_list))))]))
 
 (provide filter-odd)
 (define (filter-odd given_list)
