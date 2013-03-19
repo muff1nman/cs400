01ecec11aa47f36354c9381e196f6c9c967f46f3
Andrew DeMaria <ademaria@mines.edu>
2013-03-19 09:02:49 -0600

    implemented filter-odd and tests passing

--- MidtermReview/even_odd.rkt
+++ MidtermReview/even_odd.rkt
@@ -11,4 +11,8 @@
 
 (provide filter-odd)
 (define (filter-odd given_list)
-  "Not implemented")
+  (cond
+    [(empty? given_list) (list)]
+    [(equal? (length given_list) 1) (list (first given_list))]
+    [(>= (length given_list) 2) 
+     (list* (first given_list) (filter-odd (rest (rest given_list))))]))
