744727cf4a7d6dc0dba5bb2dd99ee9992ff4b6ea
Andrew DeMaria <ademaria@mines.edu>
2013-03-20 11:50:51 -0600

    mergelist implemented and tests passing

--- MidtermReview/merge_lists.rkt
+++ MidtermReview/merge_lists.rkt
@@ -3,4 +3,12 @@
 
 (provide mergelists)
 (define (mergelists list1 list2)
-  "TODO")
+  (cond
+    [(null? list1) list2]
+    [(null? list2) list1]
+    [else (cond
+            [(< (first list1) (first list2)) 
+              (list* (first list1) (mergelists (rest list1) list2))]
+            [else 
+              (list* (first list2) (mergelists list1 (rest list2)))])]))
+
