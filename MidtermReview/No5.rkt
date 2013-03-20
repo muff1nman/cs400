f4d07cab5ee7bc0c960a0e24df7b12480ec7908a
Andrew DeMaria <ademaria@mines.edu>
2013-03-20 16:31:35 -0600

    Trace of No5

--- MidtermReview/No5.rkt
+++ MidtermReview/No5.rkt
@@ -1,6 +1,15 @@
+#!/usr/bin/env racket
+#lang racket
+
 (define (c1 x)
   (let* [ (y 5) (z 20) (fn (lambda (y) (* x y z)))]
     (let [ (y 10) (z (+ x y z)) ]
       (fn z))))
 
+(define (c1_1 x_1)
+  (let* [ (y_2 5) (z_2 20) (fn (lambda (y_3) (* x_1 y_3 z_2)))]
+    (let [ (y_4 10) (z_3 (+ x_1 y_2 z_2))]
+      (fn z_3))))
 
+(display (c1 5))
+(display "\n")
