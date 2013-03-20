799b640d74739e3417a82888c984190802153330
Andrew DeMaria <ademaria@mines.edu>
2013-03-20 17:49:46 -0600

    More review

---
+++ MidtermReview/no23.cpp
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  no23.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/20/2013 05:36:44 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Andrew DeMaria (muff1nman), ademaria@mines.edu
+ *   Organization:  Colorado School of Mines
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+int main( int argc, char** argv) {
+    char* string;
+    string = "Fred";
+    string[2] = 'a';
+    return 0;
+}
+
