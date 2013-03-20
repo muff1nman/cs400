969f13b2fab583aacae42377fbf5ba73730a7256
Andrew DeMaria <ademaria@mines.edu>
2013-03-20 11:40:56 -0600

    Test for merge lists

---
+++ MidtermReview/test_merge_lists.rkt
@@ -0,0 +1,26 @@
+#!/usr/bin/env racket
+#lang racket
+
+(require "merge_lists.rkt")
+(require rackunit)
+
+(check-equal?
+  (mergelists '(1 5 9) '(2 3 8 11))
+  '(1 2 3 5 8 9 11)
+  "Failed for online provided example")
+
+(check-equal?
+  (mergelists '(1 5 9) '(1 3))
+  '(1 1 3 5 9)
+  "Failed for online provided example with duplicate")
+
+(check-equal?
+  (mergelists '(2 32 1) (list))
+  '(1 2 32)
+  "Failed for left empty list")
+
+(check-equal?
+  (mergelists (list) '(2 32 1) )
+  '(1 2 32)
+  "Failed for right empty list")
+
