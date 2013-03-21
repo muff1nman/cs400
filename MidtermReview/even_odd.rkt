deba55010386410fcca75a9bcf049f6199a0e9bd
Andrew DeMaria <ademaria@mines.edu>
2013-03-21 08:52:35 -0600

    Simplification of logic

--- MidtermReview/even_odd.rkt
+++ MidtermReview/even_odd.rkt
@@ -13,6 +13,6 @@
 (define (filter-odd given_list)
   (cond
     [(empty? given_list) (list)]
-    [(equal? (length given_list) 1) (list (first given_list))]
+    [(equal? (length given_list) 1) given_list]
     [(>= (length given_list) 2) 
      (list* (first given_list) (filter-odd (rest (rest given_list))))]))
