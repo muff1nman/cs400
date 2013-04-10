c84f3ce2081e26a87c4d7c0acf1ba3ee75cb8fef
Andrew DeMaria <ademaria@mines.edu>
2013-04-10 14:51:09 -0600

    Start test work for Exercise 2

---
+++ HW8/2_test.rb
@@ -0,0 +1,17 @@
+#! /usr/bin/env ruby
+#
+# Copyright © 2013 Andrew DeMaria (muff1nman) <ademaria@mines.edu>
+#
+# All Rights Reserved.
+
+require 'test/unit'
+
+class Exercise2 < Test::Unit::TestCase
+  def test_float
+  end
+
+  def test_integer
+  end
+end
+
+
