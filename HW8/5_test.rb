04474d7d2d46e8369b0957234dce1453d06246cf
Andrew DeMaria <ademaria@mines.edu>
2013-04-10 23:40:07 -0600

    Fixed a couple tests

--- HW8/5_test.rb
+++ HW8/5_test.rb
@@ -9,8 +9,8 @@ require_relative 'test_helper.rb'
 
 class Exercise5 < Test::Unit::TestCase
   def test_register_saves
-    program_assert([0,6,0,0,0,0,0,0,0,0], "{R1=2;R1*3;}", "Simple integer register save")
-    float_assert("3.0", "{R5=1.5;R5*2;}", "Simple float register save")
+    program_assert([0,2,0,0,0,0,0,0,0,0], "{R1=2;R1*3;}", "Simple integer register save")
+    program_assert([0,0,0,0,0,1.5,0,0,0,0],"{R5=1.5;R5*2;}", "Simple float register save")
   end
 
   def test_correct_register_usage
