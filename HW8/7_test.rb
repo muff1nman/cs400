e435307563cfa6f72fadbfb1506a54e0b272211f
Andrew DeMaria <ademaria@mines.edu>
2013-04-11 01:09:04 -0600

    Added unary tests

--- HW8/7_test.rb
+++ HW8/7_test.rb
@@ -8,6 +8,26 @@ require 'test/unit'
 require_relative 'test_helper.rb'
 
 class Exercise7 < Test::Unit::TestCase
+  def test_simple_unary_neg
+    float_assert( "-3", "-3", "Test simple integer unary neg")
+    float_assert( "-1.2". "-1.2", "Test simple float unary neg")
+  end
+
+  def test_unary_operator_precedence
+    float_assert("1", "4+-3", "Test simple integer add with unary")
+    float_assert("1.0", "4.0+-3.0", "Test simple float add with unary")
+    float_assert("7", "4--3", "Test simple integer subtract with unary")
+    float_assert("7.0", "4.0--3.0", "Test simple float subtract with unary")
+    float_assert("-3", "6/-2", "Test simple integer division with unary")
+    float_assert("-3.0", "6./-2.0", "Test simple float division with unary")
+    float_assert("-8", "4*-2" ,"Test simple integer multiplication with unary")
+    float_assert("-8.0", "4.0*-2.0" ,"Test simple float multiplication with unary")
+  end
+
+  def test_unary_cascade
+    float_assert("3", "----3", "Test simple integer unary neg cascade")
+    float_assert("-1.4", "---1.4", "Test simple float unary neg cascade")
+  end
 end
 
 
