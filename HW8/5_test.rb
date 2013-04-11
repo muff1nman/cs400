e290a843a1ec1f6e1c7812590a0f2de895474f7d
Andrew DeMaria <ademaria@mines.edu>
2013-04-10 22:54:24 -0600

    Tests passing! wahooo!

--- HW8/5_test.rb
+++ HW8/5_test.rb
@@ -10,15 +10,16 @@ require_relative 'test_helper.rb'
 class Exercise5 < Test::Unit::TestCase
   def test_register_saves
     float_assert("6", "{R1=2;R1*3;}", "Simple integer register save")
-    float_assert("3.0", "{R5=1.5;R1*2;}", "Simple float register save")
+    float_assert("3.0", "{R5=1.5;R5*2;}", "Simple float register save")
   end
 
   def test_correct_register_usage
+    # NOTE: The program should complain about an error.  This is expected!
     (0..4).each do |i|
-      float_not_assert("6", "{R#{i}=2;R#{i}*3;}", "Simple improper integer register#{i} save")
+      float_not_assert("3.0", "{R#{i}=1.5;R#{i}*2;}","Simple improper integer register#{i} save")
     end
     (5..9).each do |i|
-      float_not_assert("3.0", "{R#{i}=1.5;R#{i}*2;}", "Simple improper float register#{i} save")
+      float_not_assert("6", "{R#{i}=2;R#{i}*3;}", "Simple improper float register#{i} save")
     end
   end
 
