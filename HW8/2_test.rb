1189e9491caa60e210a6a76307506618ba7f9681
Andrew DeMaria <ademaria@mines.edu>
2013-04-10 18:08:15 -0600

    Added tests for mixing integers and floats

--- HW8/2_test.rb
+++ HW8/2_test.rb
@@ -29,17 +29,25 @@ class Exercise2 < Test::Unit::TestCase
   end
 
   def test_repeating_float
-
+    test("0.333333", "1.0/3.0", "Failed to do a division with repeating float as result")
   end
 
 
   def test_mixed_operations
-
+    test("7.5","4.5+3", "Failed to do a simple addition with float and integer")
+    test("7.5","3+4.5", "Failed to do a simple addition with float and integer")
+    test("2.3","3-0.7", "Failed to do a simple subtraction with float and integer")
+    test("-2.3","0.7-3", "Failed to do a simple subtraction with float and integer")
+    test("11.5","2.3*5", "Failed to do a simple multiplication with float and integer")
+    test("11.5","5*2.3", "Failed to do a simple multiplication with float and integer")
   end
 
   # singled out to ensure proper treatment of integer division
   def test_division
+    test("0.147826087","3.4/23", "Failed to do a simple division with float and integer")
+    test("12.261580381","45/3.67", "Failed to do a simple division with float and integer")
 
+    test("1", "4/3", "Failed to ensure that integer division happens")
   end
 end
 
