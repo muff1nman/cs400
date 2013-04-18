f0d878e99704d3f0eff0723eae5cb28eebf65d75
Andrew DeMaria <ademaria@mines.edu>
2013-04-17 18:05:27 -0600

    Added test files from last project; they probably all fail hard core

---
+++ HW9/6_test.rb
@@ -0,0 +1,37 @@
+#! /usr/bin/env ruby
+#
+# Copyright © 2013 Andrew DeMaria (muff1nman) <ademaria@mines.edu>
+#
+# All Rights Reserved.
+
+require 'test/unit'
+require_relative 'test_helper.rb'
+
+class Exercise6 < Test::Unit::TestCase
+  def test_simple_integer_exponent
+    float_assert( "8", "2^3", "Failed simple integer exponent")
+    assert_not_equal( formatted_result("8"), format_result(run_calculator("2^3")), "Should return float, not integer")
+    float_assert( "1", "2^0", "Failed zero exponent test")
+  end
+
+  def test_simple_float_exponent
+    float_assert( "4.342848712", "3.4^1.2", "Failed simple float exponent")
+  end
+
+  def test_float_integer_mixed_exponent
+    float_assert( "1.39038917", "3^0.3", "Failed simple mixed with float power")
+    float_assert( "92972.22253683", "1.4^34", "Failed simple mixed with integer power")
+  end
+
+  def test_operator_precedence
+    float_assert("262144", "4^3^2", "Operator precendence is wrong for only exponents")
+    float_assert("-6", "3-3^2", "Should be before subtraction")
+    float_assert("18", "2*3^2", "Should be before multiplication")
+    float_assert("0.222222", "2.0/3^2", "Should be before division")
+    float_assert("-2.444444444", "2/3^2+4^2/3+1-3^2", "Just a bunch of operations to test, all integers")
+    float_assert("-2.311474184", "3.4^1.2-8*2^1.2^0.3/3.4^0/5*2.0", "Just a bunch of operations to test, mixed float")
+
+  end
+end
+
+
---
+++ HW9/test_helper.rb
@@ -0,0 +1,41 @@
+require 'shellwords'
+def run_calculator( input )
+  `echo '#{input}' | ./calc `
+end
+
+def formatted_result( output )
+  "= #{output}\n"
+end
+
+def format_result( input )
+  input.lines.last
+end
+
+def extract_float(input)
+  input[2...-1].to_f
+end
+
+def derivation( input )
+  input.lines[0...-1].join('')
+end
+
+def float_assert( expected, calculation, error_message )
+  assert_in_epsilon( expected.to_f, extract_float(format_result(run_calculator(calculation))), 0.001, error_message)
+end
+
+def float_not_assert( expected, calculation, error_message )
+  assert_not_in_epsilon( expected.to_f, extract_float(format_result(run_calculator(calculation))), 0.001, error_message)
+end
+
+def register_values(output)
+  output.lines[-10..-1].collect do |line|
+    line[4...-1].to_f
+  end
+end
+
+def program_assert( expected_array, calculation, error_message )
+  actual_array = register_values(run_calculator(calculation))
+  (0..10).each do |i|
+    float_assert( expected_array[i].to_f, actual_array[i].to_f, error_message)
+  end
+end
